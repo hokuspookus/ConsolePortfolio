fecc41db29838be3e1caed05098d889da93bc207
Hokuspookus <68711133+hokuspookus@users.noreply.github.com>
2024-11-25 02:45:01 +0100

    vad

--- obj/Debug/net8.0/ConsolePortfolio.AssemblyInfo.cs
+++ obj/Debug/net8.0/ConsolePortfolio.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsolePortfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e95330905658c40e132e382c848149c2e25f945a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b954a1c5a046f472a698a6b1fd31533469fe79")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsolePortfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsolePortfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
